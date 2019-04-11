dd2189a065f679cd5e2e8c09c57d1a5d004bbab9
ZeIndieGame <42322229+ZeIndieGame@users.noreply.github.com>
2019-04-11 21:14:01 +0000

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
 
+player.onChat("jail", function (usr) {
+    mobs.teleportToPlayer(
+    mobs.target(TargetSelectorKind.LocalPlayer),
+    mobs.target(TargetSelectorKind.AllPlayers)
+    )
+    shapes.sphere(
+    Block.Bedrock,
+    player.position(),
+    2.1,
+    ShapeOperation.Hollow
+    )
+    player.execute(
+    "/gamemode " + usr
+    )
+})
